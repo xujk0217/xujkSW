3fec77a4353147a5f671b0f87c7ac5a6fc3d119b
許君愷 / xu jun kai <kevin16021777@gmail.com>
2024-11-17 19:19:56 +0800

    vercel setting

--- build_file.sh
+++ build_file.sh
@@ -1,7 +1,7 @@
 pip3 install -r requirements.txt
 
 # 运行数据库迁移
-python3 manage.py migrate
+# python3 manage.py migrate
 
 # 收集静态文件
-python3 manage.py collectstatic --no-input
+# python3 manage.py collectstatic --no-input
