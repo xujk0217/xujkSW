fe71eff176abc869085cd0ac3fe30db6f55aa7aa
許君愷 / xu jun kai <kevin16021777@gmail.com>
2025-08-31 23:33:06 +0800

    Fix API function format for Vercel

--- api/app-ads.txt.py
+++ api/app-ads.txt.py
@@ -1,8 +1,8 @@
-def handler(request, context):
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'text/plain',
-        },
-        'body': 'google.com, pub-4105005748617921, DIRECT, f08c47fec0942fa0\n'
-    } 
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write('google.com, pub-4105005748617921, DIRECT, f08c47fec0942fa0\n'.encode()) 
