6bebcab01520b7e1bc56107a82718c662483bdda
許君愷 / xu jun kai <kevin16021777@gmail.com>
2025-09-01 00:25:31 +0800

    Add dj-database-url configuration for Vercel deployment

--- xjkSW/settings.py
+++ xjkSW/settings.py
@@ -79,11 +79,13 @@ WSGI_APPLICATION = 'xjkSW.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
